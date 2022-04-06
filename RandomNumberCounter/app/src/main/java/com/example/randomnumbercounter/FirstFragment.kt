4da503b57a2022af4e700c08c1e5bf2be0dea731
Matthew Toomey <mtoomeydev@gmail.com>
2022-04-06 18:22:50 -0400

    initial commit, supports counting, random number generation

---
+++ RandomNumberCounter/app/src/main/java/com/example/randomnumbercounter/FirstFragment.kt
@@ -0,0 +1,84 @@
+package com.example.randomnumbercounter
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+import androidx.navigation.fragment.findNavController
+import com.example.randomnumbercounter.databinding.FragmentFirstBinding
+
+/**
+ * A simple [Fragment] subclass as the default destination in the navigation.
+ */
+class FirstFragment : Fragment() {
+
+    private var _binding: FragmentFirstBinding? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+            inflater: LayoutInflater, container: ViewGroup?,
+            savedInstanceState: Bundle?
+    ): View? {
+
+        _binding = FragmentFirstBinding.inflate(inflater, container, false)
+        return binding.root
+
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        view.findViewById<Button>(R.id.button_random).setOnClickListener {
+            val showCountTextView = view.findViewById<TextView>(R.id.textview_first)
+            val currentCount = showCountTextView.text.toString().toInt()
+            val action = FirstFragmentDirections.actionFirstFragmentToSecondFragment(currentCount)
+            findNavController().navigate(action)
+        }
+
+        view.findViewById<Button>(R.id.button_random_plus).setOnClickListener {
+            val showCountTextView = view.findViewById<TextView>(R.id.textview_first)
+            val currentCount = showCountTextView.text.toString().toInt()
+            val action = FirstFragmentDirections.actionFirstFragmentToSecondFragment(currentCount)
+            findNavController().navigate(action)
+        }
+
+        binding.msgButton.setOnClickListener {
+            // create a Toast message to display
+            val myToast = Toast.makeText(context, "Here's a message!", Toast.LENGTH_SHORT)
+            // display the message when 'message' button is clicked
+            myToast.show()
+        }
+
+        view.findViewById<Button>(R.id.count_button).setOnClickListener {
+            countMe(view)
+        }
+    }
+
+    // handle incrementing the counter when count button is clicked
+    fun countMe(view: View) {
+        // get the textview object
+        val showCountTextView = view.findViewById<TextView>(R.id.textview_first)
+
+        // get the text content of the textview object as a string
+        val countString = showCountTextView.text.toString()
+
+        // convert text content of count text to an integer and increment
+        var count = countString.toInt()
+        count++
+
+        // assign the incremented value back to the original textview
+        showCountTextView.text = count.toString()
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
---
+++ RandomNumberCounter/app/src/main/java/com/example/randomnumbercounter/SecondFragment.kt
@@ -0,0 +1,60 @@
+package com.example.randomnumbercounter
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.navigation.fragment.findNavController
+import com.example.randomnumbercounter.databinding.FragmentSecondBinding
+import androidx.navigation.fragment.navArgs
+import kotlin.random.Random
+
+/**
+ * A simple [Fragment] subclass as the second destination in the navigation.
+ */
+class SecondFragment : Fragment() {
+
+    private var _binding: FragmentSecondBinding? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+            inflater: LayoutInflater, container: ViewGroup?,
+            savedInstanceState: Bundle?
+    ): View? {
+
+        _binding = FragmentSecondBinding.inflate(inflater, container, false)
+        return binding.root
+
+    }
+
+    // define arguments passed via first fragment
+    val args: SecondFragmentArgs by navArgs()
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.buttonSecond.setOnClickListener {
+            findNavController().navigate(R.id.action_SecondFragment_to_FirstFragment)
+        }
+
+        val count = args.myArg
+        val countText = getString(R.string.random_heading, count)
+        view.findViewById<TextView>(R.id.textview_header).text = countText
+
+        var randomNumber = 0
+        if (count > 0) {
+            randomNumber = Random.nextInt(count)
+        }
+        view.findViewById<TextView>(R.id.textview_random).text = randomNumber.toString()
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
